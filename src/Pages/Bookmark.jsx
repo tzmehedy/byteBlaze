e446d1b927f10a87503aa0eb216cb88d420f637f
unknown <mehedytouhidurzaman@gmail.com>
2024-06-25 18:29:49 +0600

    navigation completed

---
+++ src/Pages/Bookmark.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmark = () => {
+    return (
+        <div>
+            <h1>This is Bookmark page</h1> 
+        </div>
+    );
+};
+
+export default Bookmark;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Nav from "../Components/Nav";
+
+
+const Home = () => {
+    return (
+      <div>
+        <div className="h-20">
+          <Nav></Nav>
+        </div>
+        <h1>This is Home page</h1>
+      </div>
+    );
+};
+
+export default Home;
